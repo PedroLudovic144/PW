e14b0fc1e1673fa8d137de1887609e4c2780c752
PedroLudovic144 <pedroludovic5@gmail.com>
2025-02-11 20:00:35 -0300

    First commit

---
+++ Pedro_Paulo/pw3-bindingsOwO/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Hello World';
+  soma =  function somar(n1: number,n2: number){
+    return n1 + n2;
+}
+}
+
+
